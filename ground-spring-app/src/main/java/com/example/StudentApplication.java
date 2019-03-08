bc22782318108896780b548533d1ab8fb485d128
MACBOOK <macbook@MBP-van-MACBOOK.lan>
2019-03-08 20:09:30 +0100

    done

--- ground-spring-app/src/main/java/com/example/StudentApplication.java
+++ ground-spring-app/src/main/java/com/example/StudentApplication.java
@@ -1,12 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 @ComponentScan({ "com.database", "com.example" })
 @SpringBootApplication
 public class StudentApplication {
